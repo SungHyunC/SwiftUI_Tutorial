04c7213adf80267b659911fbc190860395d3dfb1
SungHyunC <rlatjdgus180@naver.com>
2022-03-28 21:06:24 +0900

    0328

---
+++ SwiftUIEssential/SwiftUIEssential/SwiftUIEssentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentialApp.swift
+//  SwiftUIEssential
+//
+//  Created by 김성현 on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
